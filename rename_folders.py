28a1737132961be3286e7aad2fd5ac27cb897da0
Ofélia Jesus <ofeliajesus@gmail.com>
2020-04-16 15:59:01 +0100

    rename folders
    Script to rename folders inside a directory you need to give two arguments path and prefix.
    python3 rename_folder.py path prefix

---
+++ rename_folders.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import os, sys
+
+
+
+
+#Function to display all the name od the folders in a given directory
+def listdirs(folder):
+    return [d for d in os.listdir(folder) if os.path.isdir(os.path.join(folder, d))]
+
+
+# loop 
+#  Call the function and rename each folder add a prefix to the name
+for file in listdirs(sys.argv[1]):
+	os.rename(path + file, (path + prefix + file))
+	print (sys.argv[1] + sys.argv[2] + file)
+
+
+
+   
+
+
+
+ 
